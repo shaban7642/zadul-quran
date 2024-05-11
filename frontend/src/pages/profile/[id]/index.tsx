419779876a334043a298717fb52447864adf33cf
ashaban7642 <ashaban7642@gmail.com>
2024-05-11 19:41:56 +0300

    change-env

---
+++ frontend/src/pages/profile/[id]/index.tsx
@@ -0,0 +1,65 @@
+import type { NextPage } from 'next';
+import { useState } from 'react';
+import Box from '@mui/material/Box';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { Typography } from '@mui/material';
+import { useRouter } from 'next/router';
+import { Profile } from '../../../components/users/users-profile';
+import { DashboardLayout } from '../../../components/dashboard/dashboard-layout';
+import { AuthGuard } from '../../../components/auth/auth-guard';
+import { ChangePasswordForm } from '../../../components/users/change-password';
+
+const User: NextPage = () => {
+    const router = useRouter();
+    const { id } = router.query;
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
+    return (
+        <Box sx={{ width: '100%', typography: 'body1' }}>
+            <TabContext value={value}>
+                <Box
+                    sx={{
+                        borderBottom: 1,
+                        borderColor: 'divider',
+                        p: '10px 30px',
+                    }}
+                >
+                    <TabList
+                        variant='scrollable'
+                        scrollButtons='auto'
+                        onChange={handleChange}
+                        aria-label='lab API tabs example'
+                    >
+                        <Tab label='Profile' value='1' />
+                        <Tab label='Change Password' value='2' />
+                    </TabList>
+                </Box>
+                <TabPanel value='1'>
+                    <Typography color='inherit' variant='h4'>
+                        Profile
+                    </Typography>
+                    <Profile id={Number(id)} />
+                </TabPanel>
+                <TabPanel value='2'>
+                    <Typography color='inherit' variant='h4'>
+                        Change Password
+                    </Typography>
+                    <ChangePasswordForm id={Number(id)} />
+                </TabPanel>
+            </TabContext>
+        </Box>
+    );
+};
+User.getLayout = (page) => (
+    <AuthGuard>
+        <DashboardLayout>{page}</DashboardLayout>
+    </AuthGuard>
+);
+
+export default User;
---
+++ frontend/src/pages/roles/[id]/index.tsx
@@ -0,0 +1,28 @@
+import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+
+import { DashboardLayout } from '../../../components/dashboard/dashboard-layout';
+import { PermissionsTable } from '../../../components/roles/permissions-table';
+import { AuthGuard } from '../../../components/auth/auth-guard';
+import { OwnerGuard } from '../../../components/auth/owner-guard';
+
+const Permissions: NextPage = () => {
+    const router = useRouter();
+    const { id, role, name } = router.query;
+    return (
+        <PermissionsTable
+            role={String(role)}
+            roleId={Number(id)}
+            name={String(name)}
+        />
+    );
+};
+Permissions.getLayout = (page) => (
+    <AuthGuard>
+        <OwnerGuard>
+            <DashboardLayout>{page}</DashboardLayout>
+        </OwnerGuard>
+    </AuthGuard>
+);
+
+export default Permissions;
